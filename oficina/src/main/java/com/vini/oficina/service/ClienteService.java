b8f755cd863834294b27e052da8ee5f10f30e404
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-21 19:12:11 -0300

    feat- ClienteService

---
+++ oficina/src/main/java/com/vini/oficina/service/ClienteService.java
@@ -0,0 +1,34 @@
+package com.vini.oficina.service;
+
+import com.vini.oficina.dto.request.ClientesRequestDTO;
+import com.vini.oficina.model.entitys.Clientes;
+import com.vini.oficina.repository.ClienteRepositorie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+
+@Service
+public class ClienteService {
+    @Autowired
+    private ClienteRepositorie clienteRepositorie;
+
+    public Clientes CadastrarCliente(ClientesRequestDTO dto){
+        if (clienteRepositorie.existsByEmail(dto.getEmail())){
+            System.out.println("Email ja Existente");
+        }
+        Clientes cliente = new Clientes();
+
+        cliente.setNome(dto.getNome());
+        cliente.setEmail(dto.getEmail());
+        cliente.setCpf(dto.getCpf());
+        cliente.setTelefone(dto.getTelefone());
+        cliente.setEndereco(dto.getEndereco());
+
+        Timestamp HoraAtual = new Timestamp(System.currentTimeMillis());
+
+        cliente.setData_cadastro(HoraAtual);
+
+         return clienteRepositorie.save(cliente);
+    }
+}
