9f2ff3740db03d874e74d93ba77c1e15ea5e9308
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-21 00:29:56 -0300

    Style - tarefas

---
+++ oficina/src/main/java/com/vini/oficina/dto/response/AdministradoresResponseDTO.java
@@ -0,0 +1,7 @@
+package com.vini.oficina.dto.response;
+
+//todo esta classe vai devolver os dados para a tela swing
+//todo e nela controlar oque deve ser exibido para usuario!
+public class AdministradoresResponseDTO {
+
+}
