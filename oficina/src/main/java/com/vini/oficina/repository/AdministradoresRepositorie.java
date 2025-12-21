92e6e875f0e1947c22b22effaa03fefd6155a8aa
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-21 00:07:54 -0300

    feat - 1° Interface CRUD. mais validador de email.

---
+++ oficina/src/main/java/com/vini/oficina/repository/AdministradoresRepositorie.java
@@ -0,0 +1,10 @@
+package com.vini.oficina.repository;
+
+import com.vini.oficina.model.entitys.Administradores;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdministradoresRepositorie extends JpaRepository<Administradores,Integer> {
+    boolean existsByEmail(String email);
+}
