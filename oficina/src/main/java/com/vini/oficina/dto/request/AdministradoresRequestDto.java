b6d399993e346eed5f21c184008b60131f4cb883
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-21 00:08:34 -0300

    feat - DTO de Administradores para validação de dados que entra principalmente no email e senha

---
+++ oficina/src/main/java/com/vini/oficina/dto/request/AdministradoresRequestDto.java
@@ -0,0 +1,60 @@
+package com.vini.oficina.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class AdministradoresRequestDto {
+
+    private String nome;
+
+    private String telefone;
+
+    private String cargo;
+
+    @NotBlank(message = "email obrigatorio")
+    @Email
+    private String email;
+
+    @NotBlank
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
