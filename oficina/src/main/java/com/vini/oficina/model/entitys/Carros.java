611013922cf5e6ae0857bbcea85c96b40ee7cb76
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-23 23:50:05 -0300

    Feat - Entidada Carros.java

---
+++ oficina/src/main/java/com/vini/oficina/model/entitys/Carros.java
@@ -0,0 +1,81 @@
+package com.vini.oficina.model.entitys;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Carros {
+
+    @Id
+    @Column(name = "id_carro")
+    private int id;
+
+    private String modelo;
+
+    private String marca;
+
+    private int ano;
+
+    private String cor;
+
+    private String placa;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_cliente")
+    private Clientes clientes;
+
+    public Clientes getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
