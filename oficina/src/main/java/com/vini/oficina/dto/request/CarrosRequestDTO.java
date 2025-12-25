c61396f551f4bd4a98d1b9c98288d5d26ddef653
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-12-25 07:42:55 -0300

    feat - RequestDTO

--- oficina/src/main/java/com/vini/oficina/dto/request/CarrosRequestDTO.java
+++ oficina/src/main/java/com/vini/oficina/dto/request/CarrosRequestDTO.java
@@ -1,5 +1,67 @@
 package com.vini.oficina.dto.request;
 
-//todo finalizar
+import jakarta.validation.constraints.NotBlank;
+
 public class CarrosRequestDTO {
+    private int id;
+
+    @NotBlank(message = "Obrigatorio")
+    private String modelo;
+
+    private String marca;
+
+    private int ano;
+
+    private String cor;
+
+    @NotBlank(message = "Obrigatorio")
+    private String placa;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 }
