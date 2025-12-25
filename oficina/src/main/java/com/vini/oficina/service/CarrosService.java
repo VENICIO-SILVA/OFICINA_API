d4c00cce4a93b65030efd62616a47eeea1f4f908
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-12-25 07:43:35 -0300

    feat - Logica para cadastrar carro ligado ao ID do cliente conforme regra de negocio

--- oficina/src/main/java/com/vini/oficina/service/CarrosService.java
+++ oficina/src/main/java/com/vini/oficina/service/CarrosService.java
@@ -1,20 +1,43 @@
 package com.vini.oficina.service;
 
+import com.vini.oficina.dto.request.CarrosRequestDTO;
 import com.vini.oficina.model.entitys.Carros;
+import com.vini.oficina.model.entitys.Clientes;
 import com.vini.oficina.repository.CarrosRepositorie;
+import com.vini.oficina.repository.ClienteRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 public class CarrosService {
     //todo Finalizar parametro com DTO
     @Autowired
     private CarrosRepositorie carrosRepositorie;
+    @Autowired
+    private ClienteRepositorie clienteRepositorie;
 
-    public Carros CadastrarCarros(int id, Carros dto){
-        Carros car = new Carros();
+    public Carros CadastrarCarros(int id, CarrosRequestDTO dto) {
+        Clientes clientes = clienteRepositorie.findById(id).orElseThrow(() -> new RuntimeException("Nao encontrado"));
+        Carros carros = new Carros();
 
-        ca
-        return null;
+        carros.setModelo(dto.getModelo());
+        carros.setMarca(dto.getMarca());
+        carros.setAno(dto.getAno());
+        carros.setCor(dto.getCor());
+        carros.setPlaca(dto.getPlaca());
+        carros.setClientes(clientes);
+
+        LocalDateTime agora = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
+
+        carros.setData_Cadastro(Timestamp.valueOf(agora));
+
+        carrosRepositorie.save(carros);
+
+
+        return carros;
     }
 }
