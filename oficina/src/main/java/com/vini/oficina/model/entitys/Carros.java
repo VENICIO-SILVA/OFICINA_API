1eda992a7c974437fad0c3830f8ad24eb1456bf3
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-12-25 07:42:09 -0300

    feat - Metodos de hora de cadastro e atualização

--- oficina/src/main/java/com/vini/oficina/model/entitys/Carros.java
+++ oficina/src/main/java/com/vini/oficina/model/entitys/Carros.java
@@ -2,6 +2,8 @@ package com.vini.oficina.model.entitys;
 
 import jakarta.persistence.*;
 
+import java.sql.Timestamp;
+
 @Entity
 public class Carros {
 
@@ -23,9 +25,31 @@ public class Carros {
     @JoinColumn(name = "id_cliente")
     private Clientes clientes;
 
+    @Column
+    private Timestamp Data_Cadastro;
+    @Column()
+    private Timestamp Data_Atualizacao;
+
     public Clientes getClientes() {
         return clientes;
     }
+
+    public Timestamp getData_Cadastro() {
+        return Data_Cadastro;
+    }
+
+    public void setData_Cadastro(Timestamp data_Cadastro) {
+        Data_Cadastro = data_Cadastro;
+    }
+
+    public Timestamp getData_Atualizacao() {
+        return Data_Atualizacao;
+    }
+
+    public void setData_Atualizacao(Timestamp data_Atualizacao) {
+        Data_Atualizacao = data_Atualizacao;
+    }
+
 
     public void setClientes(Clientes clientes) {
         this.clientes = clientes;
