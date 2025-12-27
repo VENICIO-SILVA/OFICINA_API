6ca8a1618b51874142d9851ba31392a5d999c869
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-27 19:42:22 -0300

    OrdemServicoRepositorie

---
+++ oficina/src/main/java/com/vini/oficina/repository/OrdemServicoRepositorie.java
@@ -0,0 +1,7 @@
+package com.vini.oficina.repository;
+
+import com.vini.oficina.model.entities.OrdemServico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdemServicoRepositorie extends JpaRepository<OrdemServico, Integer> {
+}
