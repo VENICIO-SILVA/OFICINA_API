cc440b408aa704a18cb3403bd9db365636d8f677
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-27 19:41:58 -0300

    feat - Controller ordemServico

---
+++ oficina/src/main/java/com/vini/oficina/controller/OrdemServicoController.java
@@ -0,0 +1,10 @@
+package com.vini.oficina.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Os")
+public class OrdemServicoController {
+
+}
