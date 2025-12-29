d01a8e15f4a85f6dbfc9dd3da719f390a96f669e
Desktop <veniciodasilvaferreira9@gmail.com>
2025-12-28 22:17:29 -0300

    refactor - Alteração de variavel feat - Novas variaveis e metodos

--- oficina/src/main/java/com/vini/oficina/dto/request/OrdemServicoRequestDTO.java
+++ oficina/src/main/java/com/vini/oficina/dto/request/OrdemServicoRequestDTO.java
@@ -2,12 +2,34 @@ package com.vini.oficina.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.sql.Timestamp;
+
 public class OrdemServicoRequestDTO {
 
-    private int id;
+    private int IdCliente;
 
     @NotBlank(message = "informa numeroOS")
     private String NumeroOs;
+
+    private Timestamp DataCadastro;
+
+    private Timestamp DataAtualizacao;
+
+    public Timestamp getDataCadastro() {
+        return DataCadastro;
+    }
+
+    public void setDataCadastro(Timestamp dataCadastro) {
+        DataCadastro = dataCadastro;
+    }
+
+    public Timestamp getDataAtualizacao() {
+        return DataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Timestamp dataAtualizacao) {
+        DataAtualizacao = dataAtualizacao;
+    }
 
     public String getNumeroOs() {
         return NumeroOs;
@@ -17,11 +39,11 @@ public class OrdemServicoRequestDTO {
         NumeroOs = numeroOs;
     }
 
-    public int getId() {
-        return id;
+    public int getIdCliente() {
+        return IdCliente;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdCliente(int idCliente) {
+        this.IdCliente = idCliente;
     }
 }
