8e8e2ce9ce395fcaad8d6ba82cc62f580a796315
Desktop <veniciodasilvaferreira9@gmail.com>
2026-01-01 23:25:25 -0300

    feat - Feat add response

--- oficina/src/main/java/com/vini/oficina/service/OrdemServicoService.java
+++ oficina/src/main/java/com/vini/oficina/service/OrdemServicoService.java
@@ -1,12 +1,11 @@
 package com.vini.oficina.service;
 
 import com.vini.oficina.dto.request.OrdemServicoRequestDTO;
+import com.vini.oficina.dto.response.OrdemItemResponseDTO;
 import com.vini.oficina.dto.response.OrdemServicoResponseDTO;
-import com.vini.oficina.model.entities.Clientes;
-import com.vini.oficina.model.entities.OrdemServico;
-import com.vini.oficina.model.entities.StatusOrdemServico;
-import com.vini.oficina.model.entities.TipoOrdemServico;
+import com.vini.oficina.model.entities.*;
 import com.vini.oficina.repository.ClienteRepositorie;
+import com.vini.oficina.repository.OrdemItemRepositorie;
 import com.vini.oficina.repository.OrdemServicoRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 
 @Service
 public class OrdemServicoService {
@@ -29,6 +29,7 @@ public class OrdemServicoService {
         Clientes clientes = clienteRepositorie.findById(id).orElseThrow(() -> new RuntimeException("Cliente nao encontrado"));
         OrdemServico ordemServico = new OrdemServico();
         OrdemServicoResponseDTO ResponseDTO = new OrdemServicoResponseDTO();
+
         ordemServico.setNumeroOS(dto.getNumeroOs());
         ordemServico.setTipo(TipoOrdemServico.ORÇAMENTO);
         ordemServico.setStatus(StatusOrdemServico.ABERTA);
@@ -44,21 +45,39 @@ public class OrdemServicoService {
     }
 
     public OrdemServicoResponseDTO ObterOrdemServico(int id) {
-        OrdemServico ordemServico = ordemServicoRepositorie.findById(id).orElseThrow(() -> new RuntimeException("Ordem nao encontrada"));
-        Clientes clientes = ordemServico.getCliente();
-        OrdemServicoResponseDTO ResponseDTO = new OrdemServicoResponseDTO();
+        OrdemServico ordemServico = ordemServicoRepositorie.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ordem não encontrada"));
 
-        ResponseDTO.setCliente(clientes.getNome());
-        ResponseDTO.setIdCliente(clientes.getId());
-        ResponseDTO.setStatus(ordemServico.getTipo().toString());
-        ResponseDTO.setTipo(ordemServico.getStatus().toString());
-        ResponseDTO.setID_OS(ordemServico.getId());
-        ResponseDTO.setNumeroOs(ordemServico.getNumeroOS());
-        ResponseDTO.setObservacoes(ordemServico.getObservacoes());
-        ResponseDTO.setDataCadastro(ordemServico.getDataCadastro());
-        ResponseDTO.getDataDeAtualizacao(ordemServico.getDataAtualizacao());
-        ResponseDTO.setMao_de_obra(ordemServico.getMaoDeObra());
+        Clientes cliente = ordemServico.getCliente();
 
-        return ResponseDTO;
+        OrdemServicoResponseDTO response = new OrdemServicoResponseDTO();
+
+        response.setIdOS(ordemServico.getId());
+        response.setNumeroOs(ordemServico.getNumeroOS());
+        response.setTipo(ordemServico.getTipo().toString());
+        response.setStatus(ordemServico.getStatus().toString());
+        response.setObservacoes(ordemServico.getObservacoes());
+        response.setDataCadastro(ordemServico.getDataCadastro());
+        response.setDataDeAtualizacao(ordemServico.getDataAtualizacao());
+        response.setMaoDeObra(ordemServico.getMaoDeObra());
+
+        response.setCliente(cliente.getNome());
+        response.setIdCliente(cliente.getId());
+
+
+        List<OrdemItemResponseDTO> itensDTO = ordemServico.getOrdemServicoItens().stream().map(item -> {
+                    OrdemItemResponseDTO dto = new OrdemItemResponseDTO();
+                    dto.setIdProduto(item.getId());
+                    dto.setQuantidade(item.getQuantidade());
+                    dto.setPrecoUnitario(item.getPrecoUnitario());
+                    dto.setNomeProduto(item.getProduto().getNomeProduto());
+                    dto.setQuantidade(item.getQuantidade());
+                    return dto;
+                })
+                .toList();
+
+        response.setItems(itensDTO);
+
+        return response;
     }
 }
