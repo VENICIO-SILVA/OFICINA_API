50c52ee127288a79e5690b0b724999b31e61220a
Desktop <veniciodasilvaferreira9@gmail.com>
2026-01-08 22:06:54 -0300

    Feat - nova classe AdministradoresResponseDTO

--- oficina/src/main/java/com/vini/oficina/dto/response/AdministradoresResponseDTO.java
+++ oficina/src/main/java/com/vini/oficina/dto/response/AdministradoresResponseDTO.java
@@ -1,6 +1,70 @@
 package com.vini.oficina.dto.response;
 
 
-public class AdministradoresResponseDTO {
+import java.sql.Timestamp;
 
+public class AdministradoresResponseDTO {
+    private String nome;
+    private String Email;
+    private String Senha;
+    private String Telefone;
+    private String Cargo;
+    private Timestamp DataCadastro;
+    private Timestamp DataAtualizacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String cargo) {
+        Cargo = cargo;
+    }
+
+    public Timestamp getDataCadastro() {
+        return DataCadastro;
+    }
+
+    public void setDataCadastro(Timestamp dataCadastro) {
+        DataCadastro = dataCadastro;
+    }
+
+    public Timestamp getDataAtualizacao() {
+        return DataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Timestamp dataAtualizacao) {
+        DataAtualizacao = dataAtualizacao;
+    }
 }
