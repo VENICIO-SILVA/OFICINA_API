a8dfc8dbd080d119c08100d34a235e01f32d6582
Desktop <veniciodasilvaferreira9@gmail.com>
2026-01-08 22:05:51 -0300

    feat - Feat FindByEmail e FindByNome

--- oficina/src/main/java/com/vini/oficina/repository/AdministradoresRepositorie.java
+++ oficina/src/main/java/com/vini/oficina/repository/AdministradoresRepositorie.java
@@ -4,7 +4,12 @@ import com.vini.oficina.model.entities.Administradores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AdministradoresRepositorie extends JpaRepository<Administradores,Integer> {
     boolean existsByEmail(String email);
+    Optional<Administradores> findByEmail(String email);
+    List<Administradores> findByNomeContainingIgnoreCase(String nome);
 }
