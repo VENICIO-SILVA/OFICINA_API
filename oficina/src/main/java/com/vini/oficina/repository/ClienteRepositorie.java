5747b59ec96a606fdec0737e9ffcfa6005e933a8
Desktop <veniciodasilvaferreira9@gmail.com>
2026-01-12 23:24:41 -0300

    feat - metodos de findby

--- oficina/src/main/java/com/vini/oficina/repository/ClienteRepositorie.java
+++ oficina/src/main/java/com/vini/oficina/repository/ClienteRepositorie.java
@@ -4,8 +4,13 @@ import com.vini.oficina.model.entities.Clientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepositorie extends JpaRepository<Clientes, Integer> {
     boolean existsByEmail(String email);
+    Optional<Clientes> findByEmail(String email);
+    List<Clientes> findByNomeContainingIgnoreCase(String nome);
 }
 
