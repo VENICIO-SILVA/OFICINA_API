f8cc203419e1a3791fcf74f8ca5737b7e6edfcbf
Desktop <veniciodasilvaferreira9@gmail.com>
2026-02-12 21:30:48 -0300

    feat - metodos para consulta de id e marca do carro-

--- oficina/src/main/java/com/vini/oficina/repository/CarrosRepositorie.java
+++ oficina/src/main/java/com/vini/oficina/repository/CarrosRepositorie.java
@@ -4,7 +4,12 @@ import com.vini.oficina.model.entities.Carros;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CarrosRepositorie extends JpaRepository<Carros,Integer> {
+    Optional<Carros> findById(int id);
 
+    List<Carros> findByMarcaContainingIgnoreCase(String Marca);
 }
